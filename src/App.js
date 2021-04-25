0ab12af212767b25118acb10688b2484473c0ae1
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-04-25 12:50:32 +0600

    created consts in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import './App.css';
-import ColorDisplay from './components/ColorDisplay';
-
-import CounterDisplay from './components/CounterDisplay';
-import CounterSimpleControls from './components/CounterSimpleControls';
+const { default: ColorDisplay } = require("./components/ColorDisplay");
+const { default: ColorSimpleControls } = require("./components/ColorSimpleControls");
+const { default: CounterAdvancedControls } = require("./components/CounterAdvancedControls");
+const { default: CounterDisplay } = require("./components/CounterDisplay");
+const { default: CounterSimpleControls } = require("./components/CounterSimpleControls");
+const { default: SliderControls } = require("./components/SliderControls");
 
 function App() {
   return (
     <div className="App">
-
-      <ColorDisplay />
-      <CounterDisplay />
-      <CounterSimpleControls />
-
-
+     
+     <ColorDisplay/>
+     <ColorSimpleControls/>
+     <SliderControls/>
+     <CounterDisplay/>
+     <CounterSimpleControls/>
+     <CounterAdvancedControls/>
+     
+     
+     
     </div>
   );
 }
