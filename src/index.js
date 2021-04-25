aad68650570d313cd1fb49b0954967e8c275bf81
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-04-25 12:57:30 +0600

    index js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { createStore , combineReducers } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import counterReducers from '../src/redux/counterReducers';
+import colorReduser from '../src/redux/colorReduser';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
+import textRedusers from './redux/textRedusers';
 
-const rootReducer = combineReducers({
- counter : counterReducer,
- color : colorReducer,
-})
-const store = createStore(counterReducer)
+const rootReduser = combineReducers({
+   counter: counterReducers, 
+   color: colorReduser,
+   text: textRedusers,
+  })
+const store = createStore(rootReduser)
 
 ReactDOM.render(
   <React.StrictMode>
