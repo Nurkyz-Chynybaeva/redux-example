1aade2326ef6eb63ebb0a03c84c0d2e133d9a18d
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-04-25 13:31:16 +0600

    color reducer

--- src/redux/colorReducer.js
+++ src/redux/colorReducer.js
@@ -1,19 +1,19 @@
 
-const initialState = {
-    color: "#000000",
-  }
-  const colorReducer = (state = initialState, action) => {
+
+  const initialState = {
+    color: '#000000', 
+}
+
+const colorReduser = (state = initialState, action) => {
     const newState = { ...state };
     switch (action.type) {
-        case "set_color":
-        newState.color = action.color;
-        return newState;
-        case "range_color":
-        newState.color = action.color;
-        return newState;
-      default:
-        break;
+        
+        case "color":
+            newState.color = action.color;
+            return newState;
     }
+    
     return state;
-  }
-  export default colorReducer;
+}
+
+export default colorReduser;
