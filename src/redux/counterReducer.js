6a2bf9307dbfd2408cb05c6ae87b2687e5428d38
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-04-25 13:32:10 +0600

    counter reducer

--- src/redux/counterReducer.js
+++ src/redux/counterReducer.js
@@ -1,25 +1,36 @@
 const initialState = {
-    number: 0,
-    color : "#000000"
-  }
-  const counterReducer = (state = initialState, action) => {
-    const newState = { ...state };
-    switch (action.type) {
-      case "set_number":
-        newState.number = action.number;
-        return newState;
-        case "set_result":
-        newState.number = action.number;
-        return newState;
+  number: 0,
+}
+
+const counterReducer = (state = initialState, action) => {
+  const newState = { ...state };
+  switch (action.type) {
       case "increment_by_1":
-        newState.number++;
-        return newState;
+          return { ...state, number: state.number + 1 }
+      case "decrement_by_1":
+          return { ...state, number: state.number - 1 }
       case "increment_by_10":
-        newState.number += 10;
-        return newState;
-      default:
-        break;
-    }
-    return state;
+          return { ...state, number: state.number + 10 }
+      case "decrement_by_10":
+          return { ...state, number: state.number - 10 }
+      case "multiply_by_2":
+          return { ...state, number: state.number * 2 }
+      case "divide_by_2":
+          return { ...state, number: state.number / 2 }
+      case "reset":
+          return { ...state, number: state.number = 0}
+      case "random":
+          return { ...state, number: state.number = Math.floor(Math.random() * (1000 - 100)) + 0}
+
+      case "set_number":
+          newState.number = action.number;
+          return newState;
+      case "set_number2":
+          newState.number = action.number;
+          return newState;
   }
-  export default counterReducer;
+  
+  return state;
+}
+
+export default counterReducer;
